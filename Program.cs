3a2e77ffd8c7860612afb784e2a5b477e2c961c0
anhnguyen-lab <anhnguyen.31231021024@st.ueh.edu.vn>
2024-08-18 14:45:47 +0700

    exercise1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace exercise1
+{
+    public class bai1
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int sum = a + b;
+            Console.WriteLine(sum);//print the sum of a + b  
+        }
+    }
+}
